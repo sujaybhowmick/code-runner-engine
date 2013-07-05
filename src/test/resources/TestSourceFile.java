f250fef1c6ffc42f0a285b59fa827219e3691852
Sujay Bhowmick <sujaybhowmick@gmail.com>
2013-07-05 16:16:51 +0530

    test java code files

---
+++ src/test/resources/TestSourceFile.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sujay
+ */
+public class TestSourceFile {
+    public static void main(String args[]){
+        System.out.println("Hello Java Compiler API");
+    }
+}
